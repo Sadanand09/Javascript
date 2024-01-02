ed1d0195e05a293cb4c2c94a8d5179f9348a7ef3
Sadanand09 <sadanandgurude9@gmail.com>
2024-01-02 19:45:59 +0530

    two functions of js

---
+++ indexof.js
@@ -0,0 +1,6 @@
+const a = [1, 2, 3, 4, 5];
+console.log(a);
+const index = a.indexOf(3);
+console.log(index);
+
+
---
+++ sort.js
@@ -0,0 +1,4 @@
+const arr = ['a', 'n', 'a', 'n', 'd'];
+console.log(arr);
+arr.sort();
+console.log(arr);
